d4ddd4fa4ec18d5335495500adc7e939b11cac75
Colin Emonds <colinemonds@gmx.de>
2016-12-13 21:00:35 +0100

    clean up unused imports

--- Chequered.Test/EventTest.cs
+++ Chequered.Test/EventTest.cs
@@ -1,8 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using Chequered.Event;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 
